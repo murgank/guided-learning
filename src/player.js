32353971f47e5388985973fd921204c61de4fcfe
Murgank <murgankkumar@ymail.com>
2020-12-12 20:06:09 +0530

    Murgank - Add. Get guide data

--- src/player.js
+++ src/player.js
@@ -1,4 +1,21 @@
+const GUIDE_URL = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?";
+let guide;
+
+function getData() {
+    const $ = window.jQuery;
+    $.getJSON(
+        GUIDE_URL,
+        (json) => guide = json.data
+    );
+}
+
 (function(tag, src) {
     tag.src = src;
+    tag.type = 'text/javascript';
     document.head.appendChild(tag);
-})(document.createElement('script'), '//code.jquery.com/jquery-latest.min.js')
+    tag.onload = getData
+})(document.createElement('script'), 'https://code.jquery.com/jquery-3.5.1.min.js')
+
+module.exports = {
+    getData
+}
--- test/player.spec.js
+++ test/player.spec.js
@@ -5,6 +5,18 @@ describe('Guided learning', function () {
         require("../src/player");
 
         expect(document.head.innerHTML)
-            .toBe('<script src=\"//code.jquery.com/jquery-latest.min.js\"></script>');
+            .toBe('<script src=\"https://code.jquery.com/jquery-3.5.1.min.js\" type=\"text/javascript\"></script>');
+    });
+
+    it('should get guide json data', function () {
+        window.jQuery = require('jquery');
+        const mockJson = {
+            data: 'some-data'
+        }
+        jest.spyOn(window.jQuery,"getJSON").mockImplementation((url,callback) => callback(mockJson));
+        const { getData } = require("../src/player");
+        getData();
+
+        expect(window.jQuery.getJSON).toBeCalledTimes(1);
     });
 });
