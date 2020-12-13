c60620842a5d41bedf6b09c0135aca833c035884
Murgank <murgankkumar@ymail.com>
2020-12-13 11:53:17 +0530

    Murgank - Add. Populate dom according to the data recieved

--- src/player.js
+++ src/player.js
@@ -5,16 +5,34 @@ function getData() {
     const $ = window.jQuery;
     $.getJSON(
         GUIDE_URL,
-        (json) => guide = json.data
+        (json) => {
+            guide = json.data;
+            $("body").append("<style>" + guide.css+ "</style>"
+                +"<div  class='sttip'> " +
+                "<div class='tooltip in'> " +
+                "<div class='tooltip-arrow'></div>" +
+                "<div class='tooltip-arrow second-arrow'></div>" +
+                "<div class='popover-inner'>" +
+                guide.tiplates.tip + guide.tiplates.hoverTip+
+                "</div>" +
+                "</div>" +
+                "</div>");
+        }
     );
 }
 
-(function(tag, src) {
+(function (tag, src) {
     tag.src = src;
     tag.type = 'text/javascript';
     document.head.appendChild(tag);
     tag.onload = getData
-})(document.createElement('script'), 'https://code.jquery.com/jquery-3.5.1.min.js')
+})(document.createElement('script'), 'https://code.jquery.com/jquery-3.5.1.min.js');
+
+(function (tag, src) {
+    tag.rel = 'stylesheet';
+    tag.href = src;
+    document.head.appendChild(tag);
+})(document.createElement('link'), 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css');
 
 module.exports = {
     getData
--- test/player.spec.js
+++ test/player.spec.js
@@ -1,22 +1,33 @@
 const {describe, it} =  require("@jest/globals");
 
 describe('Guided learning', function () {
-    it('should Add Jquery to the DOM', function () {
+    it('should Add Jquery and common css to the DOM', function () {
         require("../src/player");
 
         expect(document.head.innerHTML)
-            .toBe('<script src=\"https://code.jquery.com/jquery-3.5.1.min.js\" type=\"text/javascript\"></script>');
+            .toBe('<script src=\"https://code.jquery.com/jquery-3.5.1.min.js\" type=\"text/javascript\"></script>' +
+                '<link rel=\"stylesheet\" href=\"https://guidedlearning.oracle.com/player/latest/static/css/stTip.css\">');
     });
 
-    it('should get guide json data', function () {
+    it('should get guide json data and populate dom accordingly', function () {
         window.jQuery = require('jquery');
         const mockJson = {
-            data: 'some-data'
+            data: {
+                css: 'some-css',
+                tiplates: {
+                    tip: 'some-tip',
+                    hoverTip: 'hover-tip'
+                }
+            }
         }
         jest.spyOn(window.jQuery,"getJSON").mockImplementation((url,callback) => callback(mockJson));
         const { getData } = require("../src/player");
         getData();
 
         expect(window.jQuery.getJSON).toBeCalledTimes(1);
+        expect(document.body.innerHTML).toBe('<style>some-css</style><div class="sttip">' +
+            ' <div class="tooltip in"> <div class="tooltip-arrow"></div>' +
+            '<div class="tooltip-arrow second-arrow"></div>' +
+            '<div class="popover-inner">some-tiphover-tip</div></div></div>')
     });
 });
